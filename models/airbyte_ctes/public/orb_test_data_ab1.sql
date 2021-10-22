e1022944d5fe8240befe9e628a70560f50a8a8ef
vamsikurre <vamsi.kurre@orbitanalytics.com>
2021-10-22 10:04:55 +0530

    tables corrected

--- models/airbyte_ctes/public/orb_test_data_ab1.sql
+++ models/airbyte_ctes/public/orb_test_data_ab1.sql
@@ -4,6 +4,6 @@ select
     {{ json_extract_scalar('_airbyte_data', ['random'], ['random']) }} as random,
     {{ json_extract_scalar('_airbyte_data', ['id'], ['id']) }} as {{ adapter.quote('id') }},
     _airbyte_emitted_at
-from {{ source('public', '_airbyte_raw_orb_test_data') }} as table_alias
+from {{ source('public', '_airbyte_raw_orb_inc_test_data') }} as table_alias
 -- orb_test_data
 
