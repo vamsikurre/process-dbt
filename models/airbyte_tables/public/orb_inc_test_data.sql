f7d845ee462fb65f159cf6698af316a7eacb9674
vamsikurre <vamsi.kurre@orbitanalytics.com>
2021-10-22 10:15:59 +0530

    table name updated

--- models/airbyte_tables/public/orb_inc_test_data.sql
+++ models/airbyte_tables/public/orb_inc_test_data.sql
@@ -4,7 +4,7 @@ select
     random,
     {{ adapter.quote('id') }},
     _airbyte_emitted_at,
-    _airbyte_orb_test_data_hashid
+    _airbyte_orb_inc_test_data_hashid
 from {{ ref('orb_test_data_ab3') }}
--- orb_test_data from {{ source('public', '_airbyte_raw_orb_test_data') }}
+-- orb_test_data from {{ source('public', '_airbyte_raw_orb_inc_test_data') }}
 
